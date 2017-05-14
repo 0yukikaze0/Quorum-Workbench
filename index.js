2f26e4ce70385dc395eb016d24e49d00e1cdd030
0yukikaze <neothedefender@gmail.com>
2017-05-14 17:22:37 +0530

    Added container requests

--- index.js
+++ index.js
@@ -5,5 +5,25 @@ var dockerConnect = require('./handlers/DockerConnect');
 var docker = new dockerConnect();
 //docker.getAllImages().then((images) => { console.log(images)})
 //docker.getAllContainers().then((containers) => {console.log(containers)})
+//docker.getContainersByStatus('created,restarting,running,removing,paused,exited,dead').then((containers) => {console.log(containers)})
+//docker.getContainersByStatus('created').then((containers) => {console.log(containers)})
 //docker.getNetworkListing().then((networks) => {console.log(networks)})
-docker.deleteNetwork('New Test Network').then((result) => {console.log(result)})
+//docker.createNetwork('MyTest Network').then((result) => {console.log(result)})
+//docker.deleteNetwork('MyTest Network').then((result) => {console.log(result)})
+
+var contReq = {
+    containerName : "My New Test Container",
+    image : "ubuntu:16.04",
+    tty : true,
+    detached : true,
+    cmd : [],
+    mounts : ["/home/quorum:/data/xyz"],
+    ports : {
+        "22000/tcp":[{"HostPort":"25000"}]
+    },
+    networkName : "TestNet"
+}
+
+//docker.createContainer(contReq).then((containers) => {console.log(containers)})
+//docker.startContainer('My_New_Test_Container').then((result) => {console.log(result)})
+docker.stopContainer('My_New_Test_Container').then((result) => {console.log(result)})
