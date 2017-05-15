19344eea38f5808787190f7d19760ab5d6b1149e
0yukikaze <neothedefender@gmail.com>
2017-05-15 13:31:26 +0530

    TDD placeholders

---
+++ tests/test.js
@@ -0,0 +1,11 @@
+'use strict;'
+
+var chai = require('chai');
+var should = chai.should;
+var expect = chai.expect;
+
+describe('Basic tests', () => {
+    it('placehoder', () => {
+        
+    })
+})
